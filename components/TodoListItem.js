5088682f7f357da94708544625d733228b5b68f9
Jimin Lee <jm.lee@gymmon.work>
2021-05-19 20:06:12 +0900

    add icon

--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const TodoListItem = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity>
-        <View style={styles.circle} />
+        <View style={styles.completeCircle}>
+            <Icon name="circledowno" size={30} color="#3143e8"></Icon>
+        </View>
       </TouchableOpacity>
       <Text style={styles.text}>TodoList items will be shown here</Text>
     </View>
@@ -36,6 +39,21 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     marginRight: 20,
     marginLeft: 20,
+  },
+  completeCircle: {
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  strikeText: {
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+  },
+  unstrikeText: {
+    color: '#29323c',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10,
   },
 });
 
